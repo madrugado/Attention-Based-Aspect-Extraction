284f6e0f008b4d9d45991888a81dc9ddf7f9ed6d
madrugado <valentin@maly.hk>
2018-04-05 17:32:32 +0300

    FIX: crumbles of the past

--- code/train.py
+++ code/train.py
@@ -47,7 +47,7 @@ U.mkdir_p(out_dir)
 U.print_args(args)
 
 assert args.algorithm in {'rmsprop', 'sgd', 'adagrad', 'adadelta', 'adam', 'adamax'}
-assert args.domain in {'restaurant', 'beer'}
+# assert args.domain in {'restaurant', 'beer'}
 
 if args.seed > 0:
     np.random.seed(args.seed)
